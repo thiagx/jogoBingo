eba2608b9f7e0acfc7756216dc50710ab5771ba5
Thiago Silva <eletronica.2016@hotmail.com>
2016-10-26 20:06:01 -0200

    Comentando o código

--- Bingo.java
+++ Bingo.java
@@ -1,16 +1,11 @@
-
+package trabalhoMetodo.jogoBingo;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
- *
- * @author estudos
+ * @author Thiago Oliveira Silva - Estudando do Instituto Federal de São Paulo - Campus Cubatão.
+ * Todos os direitos reservados.
  */
 public class Bingo {
 
@@ -24,30 +19,29 @@ public class Bingo {
     public static void main(String[] args) throws IOException {
         System.out.println("#### JOGO DO BINGO ####");
 
-        gerarCartela(primeiraCartela);
-        gerarCartela(segundaCartela);
+        gerarCartela(primeiraCartela);// Chamo a primeira cartela
+        gerarCartela(segundaCartela);// chamo a segunda cartela
 
-        while (rodando) {
+        while (rodando) {// enquanto rodando for verdadeiro, o jogo fica dentro de um loop
             System.out.println("=============================");
-            mostrarCartela(primeiraCartela);
+            mostrarCartela(primeiraCartela);// mostro minha primeira cartela
             System.out.println("=============================");
-            mostrarCartela(segundaCartela);
+            mostrarCartela(segundaCartela);// mostro minh segunda cartela
             System.out.println("################################");
-            sorteio();
-            verificarAcerto(primeiraCartela, 1);
-            verificarAcerto(segundaCartela, 2);
-            verificarGanhador(aux1);
-            verificarGanhador(aux2
-            );
+            sorteio();// chamo o método de sorteio para gerar um numero aleatório
+            verificarAcerto(primeiraCartela, 1);// passo primeira cartela e um ID 1 para verificar se ela acertou
+            verificarAcerto(segundaCartela, 2); // passo segunda cartela e um ID 2 para verificar se ela acertou
+            verificarGanhador(); // verifico se há um ganhador
+
         }
 
     }//############################################################################
 
-    public static void gerarCartela(int cartela[][]) throws IOException {
-
+    public static void gerarCartela(int cartela[][]) throws IOException {// metodo para gerar minhas cartelas
+// A cartela na pode haver numeros repetidos,para isso foi gerado uma booleana para verificar se achou o N repetido
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-
+// primeiro ele gera o numero, depois confere se cartela[k][l] possui aquele numero, como na linha  VERIFICANDO
                 int gerar = (int) (Math.random() * 100);
                 if (gerar == 0) {
                     gerar = (int) (Math.random() * 100);
@@ -57,7 +51,7 @@ public class Bingo {
                 for (int k = 0; k < 5; k++) {
                     for (int l = 0; l < 5; l++) {
 
-                        if (gerar == cartela[k][l]) {
+                        if (gerar == cartela[k][l]) { // VERIFICANDO
                             achou = true;
                         }
 
@@ -65,11 +59,11 @@ public class Bingo {
 
                 }
 
-                if (!achou) {
+                if (!achou) {// Se ele nao achar numero repetido, ele joga o numero dentro da cartela
 
                     cartela[i][j] = gerar;
 
-                } else {
+                } else {//caso ache, ele força eu voltar uma coluna da minha matriz
                     j--;
                 }
 
@@ -83,7 +77,7 @@ public class Bingo {
             for (int j = 0; j < 5; j++) {
                 if ((j == 2) && (i == 2)) {
                     cartela[i][j] = 00;
-                    // continue;
+                    // continue; metodo para pular uma interação. Nao esotu usando, só para recordar, caso precise utilizar
                 } else {
 
                     int valor = cartela[i][j];
@@ -108,9 +102,9 @@ public class Bingo {
 
     public static void sorteio() throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        bolinha = (int) (Math.random() * 100);
+        bolinha = (int) (Math.random() * 100);// gero minha bolinha 
 
-        if (bolinha == 0) {
+        if (bolinha == 0) {// se bolinha for igual a 0, eu forço ela gerar outro numero
             bolinha = (int) (Math.random() * 100);
         }
         if (bolinha < 10) {
@@ -124,21 +118,20 @@ public class Bingo {
             System.out.println("Acertos Cartela 2: " + aux2);
             System.out.println("");
         }
-        input.readLine();
+        input.readLine();// força-me a apertar ENTER para prosseguir o programa
 
     }//################################################################################
 
     public static void verificarAcerto(int cartela[][], int numero) {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                if (cartela[i][j] != 00) {
-                    if (cartela[i][j] == bolinha) {
-                        cartela[i][j] = 00;
-                        if (numero == 1) {
-                            aux1++;
-                        } else {
-                            aux2++;
-                        }
+
+                if (cartela[i][j] == bolinha) {// caso a posição seja igual o numero gerado, ele coloca um 00 no lugar da cartela
+                    cartela[i][j] = 00;
+                    if (numero == 1) {
+                        aux1++;// caso meu ID seja 1, ele incrementa minha variavel aux1
+                    } else {
+                        aux2++; // o mesmo caso a cima
 
                     }
                 }
@@ -146,7 +139,7 @@ public class Bingo {
         }
     }//################################################################################
 
-    public static void verificarGanhador(int auxiliares) {
+    public static void verificarGanhador() {// verifica meu ganhador atravez de condições.
         if (aux1 == 24 && aux2 == 24) {
             System.out.println("O jogo empatou!");
             rodando = false;
@@ -164,9 +157,9 @@ public class Bingo {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 if (cartela[i][j] < 10) {
-                    System.out.print("|" +  "0" + cartela[i][j] + "|  ");
+                    System.out.print("|" + "0" + cartela[i][j] + "|  ");
                 } else {
-                    System.out.print("|" +cartela[i][j] + "|  ");
+                    System.out.print("|" + cartela[i][j] + "|  ");
                 }
             }
             System.out.println("");
